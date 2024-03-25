c12d247e805d3bd6964f837cb184b531278db615
姜小鱼 <1847535232@qq.com>
2024-03-25 14:32:44 +0800

    fest：创建MainForm

---
+++ src/Form/MainForm.java
@@ -0,0 +1,137 @@
+/*
+ * Created by JFormDesigner on Mon Mar 25 14:23:20 CST 2024
+ */
+
+package Form;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * @author Administrator
+ */
+public class MainForm extends JFrame {
+    public MainForm() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        menuBar1 = new JMenuBar();
+        menu1 = new JMenu();
+        menuItem1 = new JMenuItem();
+        menu2 = new JMenu();
+        menuItem2 = new JMenuItem();
+        menuItem3 = new JMenuItem();
+        menuItem4 = new JMenuItem();
+        menu3 = new JMenu();
+        menuItem5 = new JMenuItem();
+        dialogPane = new JPanel();
+        contentPanel = new JPanel();
+        buttonBar = new JPanel();
+        okButton = new JButton();
+        cancelButton = new JButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== menuBar1 ========
+        {
+
+            //======== menu1 ========
+            {
+                menu1.setText("\u6587\u4ef6");
+
+                //---- menuItem1 ----
+                menuItem1.setText("\u9000\u51fa");
+                menu1.add(menuItem1);
+            }
+            menuBar1.add(menu1);
+
+            //======== menu2 ========
+            {
+                menu2.setText("\u7cfb\u7edf\u7ba1\u7406");
+
+                //---- menuItem2 ----
+                menuItem2.setText("\u5b66\u751f\u7ba1\u7406");
+                menu2.add(menuItem2);
+
+                //---- menuItem3 ----
+                menuItem3.setText("\u8bfe\u7a0b\u7ba1\u7406");
+                menu2.add(menuItem3);
+
+                //---- menuItem4 ----
+                menuItem4.setText("\u6210\u7ee9\u7ba1\u7406");
+                menu2.add(menuItem4);
+            }
+            menuBar1.add(menu2);
+
+            //======== menu3 ========
+            {
+                menu3.setText("\u6210\u7ee9\u5206\u6790");
+
+                //---- menuItem5 ----
+                menuItem5.setText("\u6210\u7ee9\u6392\u540d");
+                menu3.add(menuItem5);
+            }
+            menuBar1.add(menu3);
+        }
+        setJMenuBar(menuBar1);
+
+        //======== dialogPane ========
+        {
+            dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
+            dialogPane.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new BorderLayout());
+            }
+            dialogPane.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+                buttonBar.setLayout(new GridBagLayout());
+                ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 85, 80};
+                ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0};
+
+                //---- okButton ----
+                okButton.setText("OK");
+                buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- cancelButton ----
+                cancelButton.setText("Cancel");
+                buttonBar.add(cancelButton, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+                    GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0), 0, 0));
+            }
+            dialogPane.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPane, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    private JMenuBar menuBar1;
+    private JMenu menu1;
+    private JMenuItem menuItem1;
+    private JMenu menu2;
+    private JMenuItem menuItem2;
+    private JMenuItem menuItem3;
+    private JMenuItem menuItem4;
+    private JMenu menu3;
+    private JMenuItem menuItem5;
+    private JPanel dialogPane;
+    private JPanel contentPanel;
+    private JPanel buttonBar;
+    private JButton okButton;
+    private JButton cancelButton;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
